8187e54d6293a4534c9c18f2c757e31bc199ffa8
lab302-21 <lab302-21@labeng.local>
2019-01-11 16:28:26 +0700

    test

--- Lab3_3.cpp
+++ Lab3_3.cpp
@@ -3,10 +3,11 @@ using namespace std;
     int main()
     {
         int x=2;
+        float sum;
         while(x<70){
-            cout<<"1/"<<x<<"+";
-            x++;
+            x=x+1;
+            sum=1/x+sum;
         }
+        cout<<sum;
         return 0;
     }
-    
